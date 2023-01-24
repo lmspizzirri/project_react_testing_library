3fb53e8b95977c03e45f56ddaf0bb2c97b64d1c6
lmspizzirri <lm.spizzirri@gmail.com>
2023-01-24 13:56:24 -0500

    Req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o componente NotFound.js', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const request = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(request).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
